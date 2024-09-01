0dd3a8be7038ba4cc4ed06756ba74f70a3dcf428
Su Anli <anli@users.noreply.github.com>
2024-09-01 16:38:49 +0800

    refactor(features/task, entities/task): refactor pages/task (#14)

---
+++ libs/entities/task/src/index.ts
@@ -0,0 +1,4 @@
+export { type Task } from './model/task';
+export { useTask } from './model/use-task';
+export { useTasks } from './model/use-tasks';
+export { TaskList, type TaskListProps } from './ui/task-list';
